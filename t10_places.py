457232f78ce2c496a6e8c88fab885510a51766e3
AndrejsinD <david.andrejsin@gmail.com>
2019-04-30 08:03:24 -0400

    Final

--- t10_places.py
+++ t10_places.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Emily Lovell & Scott Heggen      TODO: Irfan Chowdhury, David Andrejsin
-# Username: lovelle & heggens              TODO: Chowhduryi, Andrejsind
+# Author: Emily Lovell & Scott Heggen      TODO: David Andrejsin
+# Username: lovelle & heggens              TODO: Andrejsind
 #
 # Assignment: T10: Oh, The Places You'll Go!
 #
@@ -17,18 +17,49 @@
 ####################################################################################
 
 import turtle
-
-
-class Coordinates:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+import math
 
 
 class Place:
-    def __init__(self, name, coordinates):
+    def __init__(self, person, name, coordinates, color):
         self.name = name
         self.coordinates = coordinates
+        self.person = person
+        self.color = color
+        self.gps_pos = "(0, 0)"
+
+    def __str__(self):
+        return "({0}, {1}, {2}, {3})".format(self.person, self.name, self.gps_pos, self.color)
+
+    def place_pin(self):
+        """
+        This function places a pin on the world map.
+
+        :param window: the window object where the pin will be placed
+        :param place: a tuple object describing a place to be put on the map
+        :return: None
+        """
+
+        pin = turtle.Turtle()
+        pin.penup()
+        pin.color(self.color)  # Set the pin to user's chosen color
+        pin.shape("circle")  # Sets the pin to a circle shape
+
+        # Logically, the denominator for longitude should be 360; lat should be 180.
+        # These values (195 and 120) were determined through testing to account for
+        # the extra white space on the edges of the map. You shouldn't change them!
+        pin.goto(self.coordinates[0], self.coordinates[1])
+        pin.stamp()  # Stamps on the location
+        text = "{0}'s place:\n    {1}".format(self.person, self.name)  # Setting up pin label
+        pin.write(text, font=("Arial", 10, "bold"))
+
+    def gps_location(self):
+        latitude = self.coordinates[0] * 2 / wn.window_width() * 195
+        longitude = self.coordinates[1] * 2 / wn.window_height() * 120
+        latitude = round(latitude, 6)
+        longitude = round(longitude, 6)
+        self.gps_pos = (latitude, longitude)
+        return latitude, longitude
 
 
 def parse_file(filename):
@@ -56,9 +87,36 @@ def parse_file(filename):
 
     return places_list
 
+
 def handle_mouse_event(x, y):
-    name = input("Enter the name of the place you added")
-    global
+    pin = turtle.Turtle()
+    pin.penup()
+    pin.goto(x, y)
+    pin.stamp()
+    name = input("Enter your name ")
+    place_name = input("Enter the name of your place ")
+    color = input("What is the color of your pin? ")
+    pos = pin.position()
+    pin1 = Place(name, place_name, pos, color)
+    pin1.place_pin()
+    gps_pos = pin1.gps_location()
+    print(pin1)
+    gps.append(gps_pos)
+    print("Your total trip distance is", calculate_distance(), "km")
+
+
+def calculate_distance():
+    total_distance = 0
+    if len(gps) > 1:
+        for i in range(len(gps)-1):
+            distance = 69 * math.sqrt((gps[i][0] - gps[i+1][0])**2 + (gps[i][1] - gps[i+1][1])**2)
+            total_distance = distance + total_distance
+    return total_distance
+
+
+def closer():
+    quit()
+
 
 def place_pin(window, place):
     """
@@ -131,6 +189,9 @@ def main():
     # A sample file was created for you to use here: places.txt
     in_file = input("Enter the name of your input file: ")
 
+    global wn
+    global gps
+    gps = []
     wn = turtle.Screen()
     wn.setup(width=1100, height=650, startx=0, starty=0)
     wn.bgpic("world-map.gif")
@@ -142,8 +203,14 @@ def main():
     for place in place_list:
         place_pin(wn, place)  # Adds ONE place to the map for each loop iteration
 
+    gps = []
+    wn.onclick(handle_mouse_event)
+    wn.onkey(closer, "q")
+
+    wn.listen()
+    wn.mainloop()
+
     print("Map created!")
-    # wn.exitonclick()
 
 
 if __name__ == "__main__":
